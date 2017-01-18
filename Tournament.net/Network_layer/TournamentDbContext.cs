b2eac6f7fd02c614d3bfc7d2404f51c85384c2b4
lundgren84 <lundgren84@hotmail.se>
2017-01-18 19:32:49 +0100

    Started Codefirst for Database

---
+++ Tournament.net/Network_layer/TournamentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_layer
+{
+    public class TournamentDbContext : DbContext
+    {
+        public TournamentDbContext():base("name=Tournament_DataBase")
+        {
+
+        }
+    }
+}
