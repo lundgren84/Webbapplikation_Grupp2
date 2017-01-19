ba70ba5dfcb22bd56babb3324c820798d2f1e604
lundgren84 <lundgren84@hotmail.se>
2017-01-19 18:01:40 +0100

    open/close login/register/options

--- Tournament.net/Tournament.net/Scripts/JavaScript/Hampus/Main.js
+++ Tournament.net/Tournament.net/Scripts/JavaScript/Hampus/Main.js
@@ -15,24 +15,79 @@ HL.AccountLogin_btn = $('#AccountLogin_btn');
 
 $(document).ready(function () {
     // Variables
-    HL.IndexTopDivOpen = false;
+   
+    HL.OpenInIndexDiv = "";
     // -- Clicks --
     //Options
-    HL.AccountOptions_btn.on("click", function () {     
-        getAccountPartials('/Account/Options');
+    HL.AccountOptions_btn.on("click", function () {
+        if (HL.OpenInIndexDiv === "options") {
+            HL.OpenInIndexDiv = "";
+            HL.Top_Div_In_Index.toggle('500');
+        }
+        else if (HL.OpenInIndexDiv === "") {
+            getAccountPartials('/Account/Options');
+            HL.OpenInIndexDiv = "options";
+        }
+        else {
+            HL.Top_Div_In_Index.toggle('200');
+            setTimeout(function () {
+                getAccountPartials('/Account/Options');
+            }, 220);
+
+            HL.OpenInIndexDiv = "options";
+        }
+        
     });
     //Register
     HL.AccountRegister_btn.on('click', function () {
-        getAccountPartials('/Authentication/Register');
+
+        if (HL.OpenInIndexDiv === "Register") {
+            HL.OpenInIndexDiv = "";
+            HL.Top_Div_In_Index.toggle('500');
+        }
+        else if (HL.OpenInIndexDiv === "") {
+            getAccountPartials('/Authentication/Register');
+            HL.OpenInIndexDiv = "Register";
+        }
+        else {
+            HL.Top_Div_In_Index.toggle('200');
+            setTimeout(function () {
+                getAccountPartials('/Authentication/Register');
+            }, 220);
+
+            HL.OpenInIndexDiv = "Register";
+        }
+
+
+        //getAccountPartials('/Authentication/Register');
     });
     //Login
-    HL.AccountLogin_btn.on('click', function () {
+HL.AccountLogin_btn.on('click', function () {
+
+    if (HL.OpenInIndexDiv === "Login") {
+        HL.OpenInIndexDiv = "";
+        HL.Top_Div_In_Index.toggle('500');
+    }
+    else if (HL.OpenInIndexDiv === "") {
         getAccountPartials('/Authentication/Login');
+        HL.OpenInIndexDiv = "Login";
+    }
+    else {
+        HL.Top_Div_In_Index.toggle('200');
+        setTimeout(function () {
+            getAccountPartials('/Authentication/Login');
+        }, 220);
+
+        HL.OpenInIndexDiv = "Login";
+    }
+
+    //getAccountPartials('/Authentication/Login');
+
     });
 
-    function getAccountPartials(url) {
-        HL.Top_Div_In_Index.hide();
+    function getAccountPartials(url) {    
         HL.Spinner.toggle('300');
+
         $.ajax({
             dataType: "html",
             type: "GET",
