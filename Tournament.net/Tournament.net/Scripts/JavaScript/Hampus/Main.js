48ae52becbdb8edf6b0ca484cbe9a521ba87508e
lundgren84 <lundgren84@hotmail.se>
2017-01-19 13:09:00 +0100

    Added LoadingSpinner

--- Tournament.net/Tournament.net/Scripts/JavaScript/Hampus/Main.js
+++ Tournament.net/Tournament.net/Scripts/JavaScript/Hampus/Main.js
@@ -3,6 +3,8 @@ var HL = HL || {};
 // Divs
 HL.Top_Div_In_Index = $('#Top_Div_In_Index');
 HL.Top_Div_In_Index.removeClass('hide'); HL.Top_Div_In_Index.hide();
+HL.Spinner = $('#Spinner');
+HL.Spinner.removeClass('hide'); HL.Spinner.hide();
 // Buttons
 HL.AccountOptions_btn = $('#AccountOptions_btn');
 
@@ -14,14 +16,18 @@ $(document).ready(function () {
 
     //Clicks
     HL.AccountOptions_btn.on("click", function () {
+        HL.Spinner.toggle('300');
         $.ajax({
             dataType: "html",
             type: "GET",
             url: "/Account/Options",
             success: function (data) {
+               
                 var html = data;              
                 HL.Top_Div_In_Index.html(html);
+                HL.Spinner.toggle('300');
                 HL.Top_Div_In_Index.toggle('500');
+              
             }
         });
 
