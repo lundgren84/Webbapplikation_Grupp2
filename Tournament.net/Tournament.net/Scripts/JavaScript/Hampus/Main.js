e35e1bf39de67026de72dc4810a51c5f4a0e3700
lundgren84 <lundgren84@hotmail.se>
2017-01-20 22:27:08 +0100

    Comments and simplify Hampus main.js

--- Tournament.net/Tournament.net/Scripts/JavaScript/Hampus/Main.js
+++ Tournament.net/Tournament.net/Scripts/JavaScript/Hampus/Main.js
@@ -1,98 +1,45 @@
-﻿//Namespace
+﻿// -##- Namespace -##-
 var HL = HL || {};
 
-// Divs
+// -##- Divs -##-
 HL.Top_Div_In_Index = $('#Top_Div_In_Index');
 HL.Top_Div_In_Index.removeClass('hide'); HL.Top_Div_In_Index.hide();
 HL.Spinner = $('#Spinner');
 HL.Spinner.removeClass('hide'); HL.Spinner.hide();
-// Buttons
+// -##- Buttons -##-
 HL.AccountOptions_btn = $('#AccountOptions_btn');
 HL.AccountRegister_btn = $('#AccountRegister_btn');
 HL.AccountLogin_btn = $('#AccountLogin_btn');
 
-
-
 $(document).ready(function () {
-    // Variables
-   
+    // -##- Variables -##-
     HL.OpenInIndexDiv = "";
-    // -- Clicks --
+
+    // -##- Clicks -##-
     //Options
     HL.AccountOptions_btn.on("click", function () {
-        if (HL.OpenInIndexDiv === "options") {
-            HL.OpenInIndexDiv = "";
-            HL.Top_Div_In_Index.toggle('500');
-        }
-        else if (HL.OpenInIndexDiv === "") {
-            getAccountPartials('/Account/Options');
-            HL.OpenInIndexDiv = "options";
-        }
-        else {
-            HL.Top_Div_In_Index.toggle('200');
-            setTimeout(function () {
-                getAccountPartials('/Account/Options');
-            }, 220);
-
-            HL.OpenInIndexDiv = "options";
-        }
-        
+        toggleIndexTopDiv('/Account/Options', 'options');
     });
+
     //Register
     HL.AccountRegister_btn.on('click', function () {
-
-        if (HL.OpenInIndexDiv === "Register") {
-            HL.OpenInIndexDiv = "";
-            HL.Top_Div_In_Index.toggle('500');
-        }
-        else if (HL.OpenInIndexDiv === "") {
-            getAccountPartials('/Authentication/Register');
-            HL.OpenInIndexDiv = "Register";
-        }
-        else {
-            HL.Top_Div_In_Index.toggle('200');
-            setTimeout(function () {
-                getAccountPartials('/Authentication/Register');
-            }, 220);
-
-            HL.OpenInIndexDiv = "Register";
-        }
-
-
-        //getAccountPartials('/Authentication/Register');
+        toggleIndexTopDiv('/Authentication/Register', 'Register');
     });
+
     //Login
-HL.AccountLogin_btn.on('click', function () {
-
-    if (HL.OpenInIndexDiv === "Login") {
-        HL.OpenInIndexDiv = "";
-        HL.Top_Div_In_Index.toggle('500');
-    }
-    else if (HL.OpenInIndexDiv === "") {
-        getAccountPartials('/Authentication/Login');
-        HL.OpenInIndexDiv = "Login";
-    }
-    else {
-        HL.Top_Div_In_Index.toggle('200');
-        setTimeout(function () {
-            getAccountPartials('/Authentication/Login');
-        }, 220);
-
-        HL.OpenInIndexDiv = "Login";
-    }
-
-    //getAccountPartials('/Authentication/Login');
-
+    HL.AccountLogin_btn.on('click', function () {
+        toggleIndexTopDiv('/Authentication/Login', 'Login');
     });
 
-    function getAccountPartials(url) {    
+    // -##- Functions -##-
+    // function to get html to top div in index
+    function getHtmlToTopIndexDiv(url) {
         HL.Spinner.toggle('300');
-
         $.ajax({
-            dataType: "html",
-            type: "GET",
-            url: url,
-            success: function (data) {
+            dataType: "html",  // dataType = Wath i get from the action       
+            type: "GET",   // type = Wath im gona do with the controler         
+            url: url,   // url = controler/action           
+            success: function (data) {    // if success i run this function and "data" is wath the action returns
 
                 let html = data;
                 HL.Top_Div_In_Index.html(html);
@@ -100,6 +47,23 @@ HL.AccountLogin_btn.on('click', function () {
                 HL.Top_Div_In_Index.toggle('500');
             }
         });
+    }
+    // function to toggle top div in index right
+    function toggleIndexTopDiv(url, type) {
+        if (HL.OpenInIndexDiv === type) {
+            HL.OpenInIndexDiv = "";
+            HL.Top_Div_In_Index.toggle('500');
+        }
+        else if (HL.OpenInIndexDiv === "") {
+            getHtmlToTopIndexDiv(url);
+            HL.OpenInIndexDiv = type;
+        } else {
+            HL.Top_Div_In_Index.toggle('200');
+            setTimeout(function () {
+                getHtmlToTopIndexDiv(url);
+            }, 220);
+            HL.OpenInIndexDiv = type;
+        }
     }
 
 });
