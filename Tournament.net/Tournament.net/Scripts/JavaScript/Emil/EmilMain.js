2ad49cc774c5e922fd2b77c72505717482df1486
Fagerberg90 <Fagerberg_90@hotmail.com>
2017-01-26 11:59:51 +0100

    Try to get the button and validate functions work

--- Tournament.net/Tournament.net/Scripts/JavaScript/Emil/EmilMain.js
+++ Tournament.net/Tournament.net/Scripts/JavaScript/Emil/EmilMain.js
@@ -1,14 +1,19 @@
 ﻿var EF = EF || {};
 
+EF.btnGetUser.on("click", function () {
+    CheckLogin();
+});
+
 EF.btnGetUser = $('#btnGetUser');
 
 $(document).ready(function () {
 
     EF.btnGetUser.on('click', function () {
-        console.log("click"); 
-       
+        console.log("click");
+
     });
 })
 //EF.btnGetUser.on('click', function () {
 //   ('/Authentication/Login', 'checklogin');
 //});
+
