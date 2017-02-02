297ee18b7df4805e213750a78d4c7533b68c1760
Jonas <Jonas@DESKTOP-5DF6KMC>
2017-02-02 15:45:36 +0100

    Jaser Commit

---
+++ Tournament.net/Tournament.net/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tournament.net.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 200;
+            return View("NotFound");
+        }
+
+        public ActionResult InternalServer()
+        {
+            Response.StatusCode = 200;
+            return View("InternalServer");
+        }
+    }
+}
